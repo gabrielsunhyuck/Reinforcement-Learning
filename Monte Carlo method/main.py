cc98b0f9b092860d8589cb7e68192e5f22522781
gabrielsunhyuckim <gabrielsun99@naver.com>
2025-08-31 22:22:17 +0900

    modified:   Monte Carlo method/main.py

--- Monte Carlo method/main.py
+++ Monte Carlo method/main.py
@@ -13,21 +13,27 @@ gamma = 1.0
 alpha = 0.001
 
 for k in range(50000):
-    done    = False
-    history = []
+    '''
+    50,000번의 에피소드를 반복하는 루프
+    --> 각 에피소드는 에이전트가 시작 지점에서 목표 지점까지 이동하는 한 번의 전체 탐험
+    '''
+    done    = False # 에피소드의 종료 여부를 boolean 처리 (도착 지점 도착 시 True)
+    history = []    # 하나의 에피소드동안 에이전트의 모든 행동과 결과를 저장
     
     # 각 에피소드 시작 시 환경 초기화
     x, y = w.reset()
 
     while not done:
-        # 메서드 호출
+        # 메서드 호출 (agent.py)
         action = ag.select_action()
-        # 메서드 호출
+        # 메서드 호출 (grid.py)
         (x, y), reward, done = w.step(action)
         history.append((x, y, reward))
 
-    c_reward = 0
-    # Monte Carlo 학습: history를 거꾸로 순회하며 가치 업데이트
+    c_reward = 0 # 누적 보상 초기화 (하나의 에피소드)
+    '''
+    Monte Carlo 학습: history를 거꾸로 순회하며 가치 업데이트
+    '''
     for transition in history[::-1]:
         x, y, reward = transition
         data[x][y] = data[x][y] + alpha * (c_reward - data[x][y])
